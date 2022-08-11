990da79c375e7f08cac17261caba42824f596cb9
Vanessa Bloom <ness.jade@outlook.com>
2022-08-11 14:09:00 +1000

    Categories API requests complete
    Got the Categories api route working, insomnia requests are functioning (get, post, put, delete).

---
+++ Develop/routes/api/category-routes.js
@@ -0,0 +1,100 @@
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
+
+// The `/api/categories` endpoint
+
+// find all categories
+// be sure to include its associated Products
+router.get("/", async (req, res) => {
+	try {
+		const categoriesData = await Category.findAll({
+			include: {
+				model: Product,
+			},
+		});
+		res.status(200).json(categoriesData);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+// find one category by its `id` value
+// be sure to include its associated Products
+router.get("/:id", async (req, res) => {
+	try {
+		const categoriesData = await Category.findByPk(req.params.id, {
+			include: [
+				{
+					model: Product,
+				},
+			],
+		});
+
+		if (!categoriesData) {
+			res.status(404).json({
+				message: "No category found with this id!",
+			});
+			return;
+		}
+
+		res.status(200).json(categoriesData);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+// create a new category
+router.post("/", async (req, res) => {
+	try {
+		const categoriesData = await Category.create(req.body);
+		res.status(200).json(categoriesData);
+	} catch (err) {
+		res.status(400).json(err);
+	}
+});
+
+// update a category by its `id` value
+// THIS FUNCTION BELOW NEEDS UPDATING
+router.put("/:id", async (req, res) => {
+	try {
+		const [rowsUpdated] = await Category.update(
+			{ category_name: req.body.category_name },
+			{ where: { id: req.params.id } }
+		);
+
+		if (rowsUpdated === 1) {
+			const updatedCategory = await Category.findByPk(req.params.id);
+			res.status(201).json(updatedCategory);
+		} else {
+			// Nothing was updated
+			res.status(204);
+			res.end();
+		}
+	} catch (err) {
+		res.status(400).json(err);
+	}
+});
+
+// delete a category by its `id` value
+router.delete("/:id", async (req, res) => {
+	try {
+		const categoriesData = await Category.destroy({
+			where: {
+				id: req.params.id,
+			},
+		});
+
+		if (!categoriesData) {
+			res.status(404).json({
+				message: "No categories found with this id!",
+			});
+			return;
+		}
+
+		res.status(200).json(categoriesData);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+module.exports = router;
--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # 13-Object-Relational-Mapping-ORM-E-Commerce-Back-End
+
+## Description
+
 Express.js API to use Sequelize to interact with a MySQL database. Internet retail, also known as e-commerce, is the largest sector of the electronics industry, generating an estimated $29 trillion in 2019. E-commerce platforms like Shopify and WooCommerce provide a suite of services to businesses of all sizes
+
+## User story:
+
+AS A manager at an internet retail company
+I WANT a back end for my e-commerce website that uses the latest technologies
+SO THAT my company can compete with other e-commerce companies
+
+## User setup:
+
+N/A at this stage
+
+## Planning Notes:
+
+1.
+
+## Screenshots:
+
+![Image](develop/assets/)
+
+## Links:
+
+Walkthrough video:
+GitHub: https://github.com/NessJade96/13-Object-Relational-Mapping-ORM-E-Commerce-Back-End
+
+## Commit notes:
+
+1. Copied the starter code and set up the readme.
+
+2. Went through all the files to read and understand what I have to do. Input the values (sequelize) into the models files. Set up and seeded the server - checking this on MySQL workbench.
+
+3. Went through and populated the code into the api files, most of this code is just copy and paste - I have not updated them to work inside each api file.
+
+4. Got the Categories api route working, insomnia requests are functioning (get, post, put, delete).
